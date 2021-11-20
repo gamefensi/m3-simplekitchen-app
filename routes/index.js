73f33043e7b9e3a4ead35d23596a6d756bb1e128
John M <mahan.john@gmail.com>
2021-11-20 12:00:00 -0800

    backend finished, touching up the css

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,20 @@ const Registration = mongoose.model('Registration');
 const basic = auth.basic({
   file: path.join(__dirname, '../users.htpasswd'),
 });
+router.get('/register', (req, res) => {
+  //res.send('It works!');
+  res.render('register', { title: 'Registration page' });
+});
 
 router.get('/', (req, res) => {
   //res.send('It works!');
-  res.render('form', { title: 'Registration form' });
+  res.render('index', { title: 'Home page' });
 });
 
 router.get('/registrations', basic.check((req, res) => {
   Registration.find()
     .then((registrations) => {
-      res.render('index', { title: 'Listing registrations', registrations });
+      res.render('registrants', { title: 'Listing registrations', registrations });
     })
     .catch(() => { 
       res.send('Sorry! Something went wrong.'); 
@@ -40,14 +44,14 @@ router.post('/',
         if (errors.isEmpty()) {
           const registration = new Registration(req.body);
           registration.save()
-            .then(() => {res.send('Thank you for your registration!');})
+            .then(() => {res.render('thankyou', { title: 'Thank You Page' });})
             .catch((err) => {
               console.log(err);
               res.send('Sorry! Something went wrong.');
             });
           } else {
-            res.render('form', { 
-                title: 'Registration form',
+            res.render('register', { 
+                title: 'Registration page',
                 errors: errors.array(),
                 data: req.body,
              });
